23468502cc7c24383acf23a95e4e14b91e4e7d2f
Ibrahim Khan <e.ibrahim0105@gmail.com>
2024-10-16 18:41:33 +0500

    Delete Lab work directory

--- Lab work/Lab#4/labtask#2-dateinput.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    int date=0, month=0, year=0;
-    printf("Enter your date in this format mm/dd/yyyy: ");
-    scanf("%2d/%2d/%4d",&month,&date,&year);
-    printf("You entered date: %.4d%.2d%.2d",year,month,date );
-    return 0;
-}
