c002ed8bb5f45960779269a1415b02199b6e771d
Ibrahim khan <thewordlh95@gmail.com>
2024-10-23 12:49:55 +0500

    patch

--- Lab work/Lab#5/labtask#1-guesscup.c
+++ Lab work/Lab#5/labtask#1-guesscup.c
@@ -5,13 +5,13 @@ int main (void) {
     printf("Guess number of drink conusmed: Hint (5 to 30): ");
     scanf("%d", &guess_number);
     if (guess_number == 15) {
-        printf("Woaaahh!! Great Job, you nailed it:");
+        printf("Woaaahh!! Great Job, you guessed it:");
     }
     else if (guess_number>15) {
-        printf("Stay low you have chosen larger number:");
+        printf("Stay low you have guessed larger number:");
     }
     else {
-        printf("You have chosen samller than actual number:");
+        printf("You have guessed samller than actual number:");
     }
     return 0;
 }
