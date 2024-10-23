87a45e2d9edfea366516af3458983a6a5f875f6f
Ibrahim khan <thewordlh95@gmail.com>
2024-10-23 12:51:19 +0500

    1.1

--- Lab work/Lab#5/labtask#1-guesscup.c
+++ Lab work/Lab#5/labtask#1-guesscup.c
@@ -2,7 +2,7 @@
 
 int main (void) {
     int guess_number;
-    printf("Guess number of drink conusmed: Hint (5 to 30): ");
+    printf("Guess number of cups of tea consumed: Hint (5 to 30): ");
     scanf("%d", &guess_number);
     if (guess_number == 15) {
         printf("Woaaahh!! Great Job, you guessed it:");
