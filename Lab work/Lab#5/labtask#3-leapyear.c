d2ea325cb8d5402dd1a7d88374362c24c36c1d3d
Ibrahim khan <thewordlh95@gmail.com>
2024-10-23 13:47:11 +0500

    1.33

--- Lab work/Lab#5/labtask#3-leapyear.c
+++ Lab work/Lab#5/labtask#3-leapyear.c
@@ -6,11 +6,9 @@ int main (void) {
     scanf("%4d", &year);
     if (year % 4 == 0 && year % 100 != 0) {
         printf("Year %d is a leap year", year);
-    }
-    else if (year % 4 == 0 && year % 400 == 0 ){
+    } else if (year % 4 == 0 && year % 400 == 0 ){
        printf("Year %d is a leap year", year);
-    }
-    else {
+    } else {
         printf("Year %d is not a leap year", year);
     }
     return 0;
