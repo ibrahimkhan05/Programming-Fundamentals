a8a957c27f9c94265ed28bbf6389fe0b0e643d8f
Ibrahim khan <thewordlh95@gmail.com>
2024-10-23 13:24:31 +0500

    1.3

--- Lab work/Lab#5/labtask#2-evenorodd.c
+++ Lab work/Lab#5/labtask#2-evenorodd.c
@@ -4,7 +4,7 @@ int main (void){
     int number;
     printf("Enter a number to check it's even or odd: ");
     scanf("%d", &number);
-    if (number%2==0){
+    if ((number % 2) == 0){
         printf("Your number %d is even number", number);
     } else {
         printf("Your number %d is odd number", number);
